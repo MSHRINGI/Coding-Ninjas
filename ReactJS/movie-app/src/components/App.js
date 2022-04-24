5925c4db172f897b99a325fd91c50af3eea336c3
MSHRINGI <mshringi.mec14@nituk.ac.in>
2022-04-25 00:57:53 +0530

    Complete the lecture Redux action reducer and store

--- ReactJS/movie-app/src/components/App.js
+++ ReactJS/movie-app/src/components/App.js
@@ -2,24 +2,62 @@ import React from 'react';
 import MovieCard from './MovieCard';
 import Navbar from './Navbar';
 import { data } from '../data';
+import { addMovies, setShowFavourites } from '../actions'
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className='main'>
-        <div className='tabs'>
-          <div className='tab'>Movies</div>
-          <div className='tab'>Favourites</div>
-        </div>
-        <div className='list'>
-          {data.map((movie) => (
-            <MovieCard movie={movie} />
-          ))}
+class App extends React.Component {
+  componentDidMount(){
+    const { store } = this.props;
+    console.log("Store from app.js", store);
+
+    store.subscribe(() => {
+      console.log("UPDATED");
+      this.forceUpdate();
+    })
+
+    store.dispatch(addMovies(data));
+    console.log("DISPATCHED", store.getState());
+  }
+  isMovieFavourite = (movie) => {
+    const { favourites } = this.props.store.getState();
+    const index = favourites.indexOf(movie);
+
+    if(index !== -1){
+      // movie found
+      return true;
+    }
+    return false;
+  }
+  changeTab = (val) => {
+    const { store } = this.props;
+    store.dispatch(setShowFavourites(val));
+  }
+  render(){
+    console.log("RenDER", this.props.store.getState());
+    const { list, favourites, showFavourites } = this.props.store.getState();
+    const displayMovie = showFavourites ? favourites : list
+    return (
+      <div className="App">
+        <Navbar />
+        <div className='main'>
+          <div className='tabs'>
+            <div className={`tab ${showFavourites ? '' : 'active-tabs'}`} onClick={() => this.changeTab(false)}>Movies</div>
+            <div className={`tab ${showFavourites ? 'active-tabs' : ''}`} onClick={() => this.changeTab(true)}>Favourites</div>
+          </div>
+          <div className='list'>
+            {displayMovie.map((movie, index) => (
+              <MovieCard
+                movie={movie}
+                key={`Movie-${index}`}
+                dispatch={this.props.store.dispatch}
+                isMovieFavourite={this.isMovieFavourite(movie)}
+              />
+            ))}
+          </div>
+          { displayMovie.length === 0 ? <div className='no-movies'>No movies to display!</div> : null}
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- ReactJS/movie-app/src/components/MovieCard.js
+++ ReactJS/movie-app/src/components/MovieCard.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { addFavourite, removeFromFavourites } from "../actions";
 
 class MovieCard extends React.Component {
-    render() {
+    handleFavouriteClick = () => {
         const { movie } = this.props;
+        this.props.dispatch(addFavourite(movie));
+    }
+    handleUnFavouriteClick = () => {
+        const { movie } = this.props;
+        this.props.dispatch(removeFromFavourites(movie));
+    }
+    render() {
+        const { movie, isMovieFavourite } = this.props;
         return (
             <div className="movie-card">
                 <div className="left">
@@ -13,7 +22,10 @@ class MovieCard extends React.Component {
                     <div className="plot"> { movie.Plot } </div>
                     <div className="footer">
                         <div className="rating"> { movie.imdbRating } </div>
-                        <button className="favourite-btn">Favourite</button>
+                        { isMovieFavourite
+                            ? <button className="unfavourite-btn" onClick={this.handleUnFavouriteClick}>UnFavourite</button>
+                            : <button className="favourite-btn" onClick={this.handleFavouriteClick}>Favourite</button>
+                        }
                     </div>
                 </div>
             </div>
